e0ab219c5f24f75c2d3678320ca15439a8bf4659
yassingamalz <67608782+yassingamalz@users.noreply.github.com>
2023-10-31 00:05:03 +0100

    Delete UI/baqalti directory

--- UI/baqalti/android/app/src/main/kotlin/com/example/baqalti/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.baqalti
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- UI/baqalti/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:baqalti/my_app.dart';
-
-void main() {
-  runApp(MyApp());
-}
